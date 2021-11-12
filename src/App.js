97d2a917e1b41ef7c5584ddcfbacecd7f049ab8b
lismomo <93498377+lismomo@users.noreply.github.com>
2021-11-12 10:57:31 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
